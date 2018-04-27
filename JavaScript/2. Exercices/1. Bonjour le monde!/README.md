bf419b842c565499d3c82a353fb644012eabc8cf
Cloutier <Sylvain.Cloutier2@saaq.gouv.qc.ca>
2018-04-27 11:44:08 -0400

    Présentation et exercices débutés

---
+++ JavaScript/2. Exercices/1. Bonjour le monde!/README.md
@@ -0,0 +1,17 @@
+# Exercice 1 - Bonjour le monde!
+Cet exercice a pour but de vous familliariser avec le concept de séparation du JS et de la vue.
+
+_Outils nécessaires_
+* Git
+* Notepad++
+* Un fureteur
+
+_Connaissances acquises_
+1. Comprendre comment séparer le code comportemental du code de présentation.
+2. Utiliser les API de journalisation des navigateurs.
+
+## À faire
+1. Éditer le fichier `index.js` pour ajouter de la journalisation :
+    * Information
+	* Avertissement
+	* Erreur
---
+++ JavaScript/2. Exercices/1. Bonjour le monde!/index.js
@@ -0,0 +1,38 @@
+/*
+	Hello :)
+	
+	L'objet console donne accès à la console de débogage du navigateur 
+	(par exemple : la Console Web dans Firefox). Les spécificités de 
+	fonctionnement varient d'un navigateur à l'autre, mais il y a tout 
+	de même un ensemble de fonctionnalités qui sont fournies de base.
+	
+	console.log()
+		Permet d'afficher des messages dans la console. Vous pouvez 
+		utiliser les caractères de substitution et des arguments 
+		supplémentaires avec cette méthode.
+	
+	ex.: 
+		console.log('Hello world!');		// Hello World!
+		console.log('Hello %s!', 'Tony');	// Hello Tony!
+		
+	console.warn()
+		Outputs a warning message. You may use string substitution 
+		and additional arguments with this method.
+		
+	ex.: 
+		console.warn('Hello world!');		// Hello World!
+		console.warn('Hello %s!', 'Tony');	// Hello Tony!
+		
+	console.error()
+		Outputs an error message. You may use string substitution 
+		and additional arguments with this method.
+	
+	ex.: 
+		console.error('Hello world!');		// Hello World!
+		console.error('Hello %s!', 'Tony');	// Hello Tony!
+	
+*/
+
+console.log('Hello World!');
+console.warn('Hello World!');
+console.error('Hello World!');
